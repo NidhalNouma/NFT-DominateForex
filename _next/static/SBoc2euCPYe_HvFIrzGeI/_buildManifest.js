c146a068709d3ee1a015c1f28e1bb8cf6a84ed1c
NidhalNouma <55117372+NidhalNouma@users.noreply.github.com>
2021-12-16 21:31:20 +0000

    Deploying to gh-pages from  @ 163c7f24ed1ee383f19dcbfaeb888da0d25749e9 🚀

---
+++ _next/static/SBoc2euCPYe_HvFIrzGeI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-dd0ea8a709c1803e.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/member":["static/chunks/pages/member-3f19cd3728095ac4.js"],sortedPages:["/","/_app","/_error","/member"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
